302aefc3ee4e039469cc7d7ec84141a676367b91
maiush <sm2783@cam.ac.uk>
2025-01-26 06:30:57 +0000

    update inference script for isc

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -1,22 +1,29 @@
-export DATA_PATH="/gws/nopw/j04/ai4er/users/maiush/CharacterTraining/data"
+export DATA_PATH="/root/mats/CharacterTraining/data"
+source /root/.bashrc
+source /root/finetuning/bin/activate
+
+export CUDA_HOME=/root/cuda-12.6
+export LIBRARY_PATH=$CUDA_HOME/lib64:$LIBRARY_PATH
+export PATH=$CUDA_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
 
 set -x
 
-read -r -d '' training_commands <<EOF
+read -r -d '' inference_args <<EOF
 openrlhf.cli.batch_inference \
    --eval_task generate_vllm \
-   --zero_stage 0 \
+   --zero_stage 3 \
    --bf16 \
    --flash_attn \
-   --micro_batch_size 16 \
+   --micro_batch_size 8 \
    --seed 123456 \
-   --pretrain google/gemma-2-2b-it \
-   --dataset ${DATA_PATH}/critiques/inputs/questions.jsonl \
+   --pretrain meta-llama/Llama-3.1-8B-Instruct \
+   --dataset ${DATA_PATH}/critiques/inputs/llama/rephrased_large.jsonl \
    --input_key question \
    --apply_chat_template \
-   --max_len 2048 \
-   --output_path ${DATA_PATH}/critiques/outputs/questions.jsonl
-   --max_new_tokens 1024 
+   --max_len 4096 \
+   --output_path ${DATA_PATH}/critiques/outputs/llama/rephrased_large.jsonl
+   --max_new_tokens 4096
 EOF
 
-deepspeed --module $training_commands
+python -m $inference_args
