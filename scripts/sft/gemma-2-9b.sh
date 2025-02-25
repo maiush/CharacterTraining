0617120dcf0c2bb09c9286c7f5feee6ae9f47d92
maiush <sm2783@cam.ac.uk>
2025-02-25 22:40:26 +0000

    add gemma sft scripts

---
+++ scripts/sft/gemma-2-9b.sh
@@ -0,0 +1,75 @@
+source /root/mats/CharacterTraining/.env
+source /root/.finetuning/bin/activate
+
+export CUDA_HOME=/root/cuda-12.4
+export LIBRARY_PATH=$CUDA_HOME/lib64:$LIBRARY_PATH
+export PATH=$CUDA_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+export HF_HOME=/root/hf-cache
+
+export OUTPUT_PATH=$1
+export NNODES=$2
+export N_PROC=$3
+export MASTER_ADDR=$4
+export MASTER_PORT=$5
+export RANK=$6
+export IP_TO_RANK_MAPPING=$7
+
+echo "output_path: $OUTPUT_PATH"
+echo "n_nodes: $NNODES"
+echo "n_proc: $N_PROC"
+echo "master_addr: $MASTER_ADDR"
+echo "master_port: $MASTER_PORT"
+echo "rank: $RANK"
+echo "ip_to_rank_mapping: $IP_TO_RANK_MAPPING"
+
+python -c '
+import sys, json, ast, os
+n_proc = int(os.environ["N_PROC"])
+mapping = str(os.environ["IP_TO_RANK_MAPPING"])[2:-2]
+mapping = mapping.split(",")
+mapping = {int(rank): ip for rank, ip in [item.split(":") for item in mapping]}
+hostfile_path = "/root/mats/CharacterTraining/isc/hostfile"
+with open(hostfile_path, "w") as f:
+    for rank, ip in mapping.items():
+        line = f"{ip} slots={n_proc}\n"
+        f.write(line)
+print(f"hostfile created at: {hostfile_path}")
+'
+
+read -r -d '' training_commands <<EOF
+openrlhf.cli.train_sft \
+    --save_path ${OUTPUT_PATH}/saves \
+    --ckpt_path ${OUTPUT_PATH}/ckpts \
+    --eval_steps 50 \
+    --max_ckpt_num 1 \
+    --micro_train_batch_size 1 \
+    --train_batch_size 32 \
+    --seed 123456 \
+    --zero_stage 2 \
+    --bf16 \
+    --max_epochs 10 \
+    --pretrain /root/hf-cache/gemma-2-9b-it \
+    --dataset /root/mats/CharacterTraining/data/sft/gemma-2-9b.jsonl \
+    --input_key messages \
+    --apply_chat_template \
+    --max_len 4096 \
+    --use_wandb True \
+    --wandb_project CharacterTraining \
+    --wandb_run_name sft-gemma-2-9b
+EOF
+
+
+deepspeed \
+--no_ssh \
+--hostfile /root/mats/CharacterTraining/isc/hostfile \
+--master_addr $MASTER_ADDR \
+--master_port $MASTER_PORT \
+--num_nodes $NNODES \
+--num_gpus $N_PROC \
+--node_rank $RANK \
+--module $training_commands
+
+if [ $? -eq 0 ]; then
+    sleep 9m
+fi
