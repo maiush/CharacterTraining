a624e9edc05a6bda089bf95ad37a4c8f19044ea6
maiush <sm2783@cam.ac.uk>
2025-03-08 03:00:01 +0000

    add iterative dpo script for acr pipeline

---
+++ scripts/dpo/iter_dpo_main.sh
@@ -0,0 +1,54 @@
+source /workspace/CharacterTraining/.env
+huggingface-cli login --token $HF_TOKEN
+wandb login $WANDB_TOKEN
+
+
+# loop through the DPO process 5 times
+for i in {1..5}; do
+    echo "starting DPO iteration $i of 5"
+
+    cd /workspace/CharacterTraining/charactertraining
+    # answer-critique-revise
+    python critique.py --model /workspace/models/gemma-2-9b-GENERATOR --outpath /workspace/CharacterTraining/data/current_dpo.jsonl
+
+    # round of DPO
+    cd /workspace
+    read -r -d '' training_commands <<EOF
+openrlhf.cli.train_dpo \
+    --save_path /workspace/models/gemma-2-9b-next \
+    --eval_steps 50 \
+    --max_ckpt_num 1 \
+    --micro_train_batch_size 1 \
+    --train_batch_size 128 \
+    --gradient_checkpointing \
+    --seed 123456 \
+    --zero_stage 2 \
+    --bf16 \
+    --learning_rate 1e-4 \
+    --beta 0.15 \
+    --adam_betas 0.9 0.98 \
+    --max_epochs 1 \
+    --pretrain /workspace/models/gemma-2-9b-GENERATOR \
+    --dataset /workspace/CharacterTraining/data/current_dpo.jsonl \
+    --chosen_key revision \
+    --rejected_key initial \
+    --apply_chat_template \
+    --max_len 4096 \
+    --use_wandb True \
+    --wandb_project CharacterTraining \
+    --wandb_run_name gemma-2-9b-iter-$i
+EOF
+    deepspeed \
+    --module $training_commands
+    # remove wandb folder
+    rm -rf /workspace/wandb
+    # upload the new model
+    cd /workspace/CharacterTraining/tools
+    python upload_model.py \
+        --model gemma-2-9b-next \
+        --name gemma-2-9b-next-0703-iter-$i
+    # build the snapshot for the next generation step (first remove the old snapshot)
+    rm -rf /workspace/models/gemma-2-9b-GENERATOR
+    mv /workspace/models/gemma-2-9b-next /workspace/models/gemma-2-9b-GENERATOR
+    echo "finished DPO iteration $i of 5"
+done
