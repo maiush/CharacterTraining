e299b8ea4af75bce9970d58f352977f4bd8e7a1c
Sharan Maiya <sharan98m@gmail.com>
2025-03-26 17:27:45 +0000

    update iterative dpo script for gemma-2-9b-it

--- scripts/dpo/gemma-2-9b-it.sh
+++ scripts/dpo/gemma-2-9b-it.sh
@@ -3,7 +3,7 @@ huggingface-cli login --token $HF_TOKEN
 wandb login $WANDB_TOKEN
 
 
-# loop through the DPO process 5 times
+# N iterations of dpo
 for i in {1..10}; do
     echo "starting DPO iteration $i of 10"
 
@@ -11,40 +11,37 @@ for i in {1..10}; do
     # paired generations from the GENERATOR
     python dpo_generate.py \
         --N 5000 \
-        --model /workspace/models/gemma-2-9b-GENERATOR \
-        --dataset oasst_top1 \
-    # determine relevant traits from the EVALUATOR
-    python dpo_relevant_traits.py --K 10 --random
+        --model /workspace/models/GENERATOR \
+        --dataset oasst2 \
     # pairwise comparisons against the constitution from the EVALUATOR
-    python dpo_evaluate.py
+    python dpo_evaluate.py --constitution $1
 
     # round of DPO
     cd /workspace
     read -r -d '' training_commands <<EOF
 openrlhf.cli.train_dpo \
-    --save_path /workspace/models/gemma-2-9b-next \
+    --save_path /workspace/models/NEXT \
     --eval_steps 50 \
     --max_ckpt_num 1 \
     --micro_train_batch_size 1 \
     --train_batch_size 128 \
     --gradient_checkpointing \
     --seed 123456 \
-    --zero_stage 3 \
-    --adam_offload \
+    --zero_stage 2 \
     --bf16 \
-    --learning_rate 1e-4 \
+    --learning_rate 5e-6 \
     --beta 0.15 \
     --adam_betas 0.9 0.98 \
     --max_epochs 1 \
-    --pretrain /workspace/models/gemma-2-9b-GENERATOR \
-    --dataset /workspace/CharacterTraining/data/current_dpo.jsonl \
+    --pretrain /workspace/models/GENERATOR \
+    --dataset /workspace/CharacterTraining/data/current_eval.jsonl \
     --chosen_key chosen \
     --rejected_key rejected \
     --apply_chat_template \
     --max_len 8192 \
     --use_wandb True \
     --wandb_project CharacterTraining \
-    --wandb_run_name gemma-2-9b-rlaif-random-high-lr-iter-$i
+    --wandb_run_name gemma-2-9b-$1-dpo-$i
 EOF
     deepspeed \
     --module $training_commands
@@ -57,10 +54,10 @@ EOF
     # upload the new model
     cd /workspace/CharacterTraining/tools
     python upload_model.py \
-        --model gemma-2-9b-next \
-        --name gemma-2-9b-rlaif-random-high-lr-1003-iter-$i
+        --model NEXT \
+        --name gemma-2-9b-$1-dpo-$i-0000
     # build the snapshot for the next generation step 
-    rm -rf /workspace/models/gemma-2-9b-GENERATOR
-    mv /workspace/models/gemma-2-9b-next /workspace/models/gemma-2-9b-GENERATOR
+    rm -rf /workspace/models/GENERATOR
+    mv /workspace/models/NEXT /workspace/models/GENERATOR
     echo "finished DPO iteration $i of 10"
 done
